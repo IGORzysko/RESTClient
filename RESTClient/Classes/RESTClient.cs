d1d2f3ea03e46052618e78737dbec63e42ea6651
Igor Trafalski <igorek2691@gmail.com>
2017-10-29 19:04:00 +0100

    Add project files.

---
+++ RESTClient/Classes/RESTClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTClient.Classes
+{
+    internal class RESTClient : IRESTClient
+    {
+        public RESTClient ()
+        {
+
+        }
+
+        // implement structure here ...
+    }
+}
