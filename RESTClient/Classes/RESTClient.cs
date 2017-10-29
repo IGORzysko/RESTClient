74abc59dd9f8a803adf1e0660ddd4a254e422eba
Igor Trafalski <igorek2691@gmail.com>
2017-10-29 21:12:56 +0100

    dodanie klasy HTTPMethod z enumem httpMethod ; dodanie niezaimplementowanej metody MakeRequest w klasie RESTClient

--- RESTClient/Classes/RESTClient.cs
+++ RESTClient/Classes/RESTClient.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RESTClient.Classes;
 
 namespace RESTClient.Classes
 {
     internal class RESTClient : IRESTClient
     {
+        private string endpoint;
+
+        string Endpoint
+        {
+            get
+            {
+                return endpoint;
+            }
+            set
+            {
+                endpoint = value;
+            }
+        }
+
+        private HttpMethodEnum httpMethodEnum;
+
+        HttpMethodEnum HttpMethodEnum
+        {
+            get
+            {
+                return httpMethodEnum;
+            }
+            set
+            {
+                httpMethodEnum = value;
+            }
+        }
+
         public RESTClient ()
         {
 
         }
+        
+        public string MakeRequest ()
+        {
+            // implement logic here ...
 
-        // implement structure here ...
+            return "";
+        }
+
     }
 }
